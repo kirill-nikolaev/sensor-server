7a3a77ffcd0c477c861c79197ea50338946d9f13
Kirill Nikolaev <kirill.nikolaev.an@gmail.com>
2022-10-22 21:53:07 +0300

    init commit

---
+++ src/main/java/com/example/SensorRestAPI/SensorsController.java
@@ -0,0 +1,11 @@
+package com.example.SensorRestAPI;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sensors")
+public class SensorsController {
+
+}
---
+++ src/test/java/com/example/SensorRestAPI/SensorRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SensorRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
